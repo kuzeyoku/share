c67df083d4fa2ce4b78b93b5fdb7b27949829753
Kuzey Oku Software <171201010@ogrenci.karatekin.edu.tr>
2024-08-27 00:31:04 +0300

    a

--- public/assets/theme/js/script.js
+++ public/assets/theme/js/script.js
@@ -62,8 +62,8 @@
       autoplay: true,
       autoplayTimeout: 7000,
       navText: [
-        '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"><path d="m31.71 15.29-10-10-1.42 1.42 8.3 8.29H0v2h28.59l-8.29 8.29 1.41 1.41 10-10a1 1 0 0 0 0-1.41z" data-name="3-Arrow Right"/></svg>',
-        '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"><path d="m31.71 15.29-10-10-1.42 1.42 8.3 8.29H0v2h28.59l-8.29 8.29 1.41 1.41 10-10a1 1 0 0 0 0-1.41z" data-name="3-Arrow Right"/></svg>',
+        '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32"><path d="m31.71 15.29-10-10-1.42 1.42 8.3 8.29H0v2h28.59l-8.29 8.29 1.41 1.41 10-10a1 1 0 0 0 0-1.41z" data-name="3-Arrow Right"/></svg>',
+        '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32"><path d="m31.71 15.29-10-10-1.42 1.42 8.3 8.29H0v2h28.59l-8.29 8.29 1.41 1.41 10-10a1 1 0 0 0 0-1.41z" data-name="3-Arrow Right"/></svg>',
       ],
       responsive: {
         0: {
