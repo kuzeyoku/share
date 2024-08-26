f79a73b2636aecb03b55707d38e44d6518d5f1c3
Kuzey Oku Software <171201010@ogrenci.karatekin.edu.tr>
2024-08-26 14:41:05 +0300

    home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -25,23 +25,9 @@ class HomeController extends Controller
             return Product::active()->order()->limit(10)->get();
         });
 
-        $data["project"] = Cache::remember("project_home_" . app()->getLocale(), config("cache.time"), function () {
-            return Project::active()->order()->get();
-        });
-
         $data["blog"] = Cache::remember("blog_home_" . app()->getLocale(), config("cache.time"), function () {
             return Blog::active()->order()->limit(3)->get();
         });
-
-        $data["reference"] = Cache::remember("reference_home_" . app()->getLocale(), config("cache.time"), function () {
-            return Reference::active()->order()->get();
-        });
-
-        if (config("information.about_page")) {
-            $data["about"] = Cache::remember("about_home_" . app()->getLocale(), config("cache.time"), function () {
-                return Page::findOrFail(config("information.about_page"));
-            });
-        }
         return view("index", $data);
     }
 }
