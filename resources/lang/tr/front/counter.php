370c8ed9a1262224efcc961765ae224ea9cda939
Kuzey Oku Software <171201010@ogrenci.karatekin.edu.tr>
2024-08-31 00:04:02 +0300

    df

---
+++ resources/lang/tr/front/counter.php
@@ -0,0 +1,19 @@
+<?php
+
+return
+    [
+        "txt1" => "Geografik | Coğrafi Bilgi Teknolojileri",
+        "txt2" => "3D Haritalama İçin En İyi Çözüm",
+        "txt3" => "2016",
+        "txt4" => "Kuruluş",
+        "txt5" => "8000",
+        "txt6" => "Kullanıcı",
+        "txt7" => "50",
+        "txt8" => "Patent",
+        "txt9" => "40",
+        "txt10" => "Uygulama",
+        "txt11" => "İletişim Bilgilerimiz",
+        "txt12" => "Tel :",
+        "txt13" => "Email :",
+        "txt14" => "Adres :",
+    ];
--- resources/views/layout/counter.blade.php
+++ resources/views/layout/counter.blade.php
@@ -3,37 +3,40 @@
         <div class="row">
             <div class="col-lg-6 wow fadeInLeft" data-wow-delay="800">
                 <h2>
-                    SHAREUAV LTD
+                    @lang('front/counter.txt1')
                 </h2>
                 <h3>
-                    Ideal for 3D Mapping
+                    @lang('front/counter.txt2')
                 </h3>
                 <div class="counter-content">
                     <div class="row">
                         <div class="col-lg-3 col-sm-6">
-                            <span>2016</span>
-                            <p>Established</p>
+                            <span>@lang('front/counter.txt3')</span>
+                            <p>@lang('front/counter.txt4')</p>
                         </div>
                         <div class="col-lg-3 col-sm-6">
-                            <span>8000</span><figure>+</figure>
-                            <p>End users</p>
+                            <span>@lang('front/counter.txt5')</span>
+                            <figure>+</figure>
+                            <p>@lang('front/counter.txt6')</p>
                         </div>
                         <div class="col-lg-3 col-sm-6">
-                            <span>50</span><figure>+</figure>
-                            <p>Patents</p>
+                            <span>@lang('front/counter.txt7')</span>
+                            <figure>+</figure>
+                            <p>@lang('front/counter.txt8')</p>
                         </div>
                         <div class="col-lg-3 col-sm-6">
-                            <span>40</span><figure>+</figure>
-                            <p>Application</p>
+                            <span>@lang('front/counter.txt9')</span>
+                            <figure>+</figure>
+                            <p>@lang('front/counter.txt10')</p>
                         </div>
                     </div>
                 </div>
                 <h2 class="b_border">
-                    Contact Us
+                    @lang('front/counter.txt11')
                 </h2>
-                <p>Tel : +90 545 280 12 43</p>
-                <p>Email : info@shareuavtec.com.tr</p>
-                <p>Address : 1000 Lakes Drive, Suite 400, West Covina, CA 91790</p>
+                <p>@lang('front/counter.txt12') {{ config('contact.phone') }}</p>
+                <p>@lang('front/counter.txt13') {{ config('contact.email') }}</p>
+                <p>@lang('front/counter.txt14') {{ config('contact.address') }}</p>
             </div>
             <div class="col-lg-6 counter-banner wow fadeInRight" data-wow-delay="800">
                 <img src="https://cdn-cloudflare.meidianbang.cn/comdata/81833/202109/202109271834187d5196.jpg"
