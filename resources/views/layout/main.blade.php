cccf33a7b2adc7beb6b0a5dca247c19cb780b68f
Kuzey Oku Software <171201010@ogrenci.karatekin.edu.tr>
2024-08-30 17:41:06 +0300

    a

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     {!! SEO::generate() !!}
-    <link rel="icon" type="image/png" href="{{ $themeAsset->favicon }}" />
+    <link rel="icon" type="image/x-icon" href="{{ $themeAsset->favicon }}" />
     <link href="https://fonts.googleapis.com/css2?family=Archivo:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Titillium+Web:ital,wght@0,200;0,300;0,400;0,600;0,700;0,900;1,200;1,300;1,400;1,600;1,700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="{{ themeAsset('front', 'css/bootstrap.min.css') }}" />
