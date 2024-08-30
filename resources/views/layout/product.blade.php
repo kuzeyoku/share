574127bac4aee073f3805770a332434862d07573
Kuzey Oku Software <171201010@ogrenci.karatekin.edu.tr>
2024-08-31 00:46:16 +0300

    p

--- resources/views/layout/product.blade.php
+++ resources/views/layout/product.blade.php
@@ -2,8 +2,19 @@
     <div class="container-fluid">
         <div clasws="services-one__inner">
             <div class="row">
-                @foreach ($product as $product)
+                {{-- @foreach ($product as $product)
                     <div class="col-lg custom-col">
+                        <div class="product-container">
+                            <a href="{{ $product->url }}">
+                                <img width="300" height="300" class="product-image"
+                                    src="{{ $product->getFirstMediaUrl('cover') }}" alt="">
+                            </a>
+                            <p class="product-title">{{ $product->title }}</p>
+                        </div>
+                    </div>
+                @endforeach --}}
+                @foreach ($product as $product)
+                    <div class="col-lg-3">
                         <div class="product-container">
                             <a href="{{ $product->url }}">
                                 <img width="300" height="300" class="product-image"
