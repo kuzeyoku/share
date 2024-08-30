51037f8b6fefaaeb6208d37e80a0367161138bef
Kuzey Oku Software <171201010@ogrenci.karatekin.edu.tr>
2024-08-31 01:14:37 +0300

    c

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -3,6 +3,43 @@
     @include('layout.slider')
     <section class="contact-three">
         <div class="container">
+            <div class="row">
+                <!--Contact Two Single Start-->
+                <div class="col-xl-4 col-lg-4">
+                    <div class="contact-two__single">
+                        <div class="contact-two__icon">
+                            <span class="icon-call"></span>
+                        </div>
+                        <p>İletişim Telefon</p>
+                        <h3><a href="tel:{{ config("contact.phone") }}">{{ config("contact.phone") }}</a></h3>
+                        <p></p>
+                    </div>
+                </div>
+                <!--Contact Two Single End-->
+                <!--Contact Two Single Start-->
+                <div class="col-xl-4 col-lg-4">
+                    <div class="contact-two__single">
+                        <div class="contact-two__icon">
+                            <span class="icon-envelope"></span>
+                        </div>
+                        <p>İletişim E-Posta</p>
+                        <h3><a href="mailto:{{ config("contact.email") }}">{{ config("contact.email") }}</a></h3>
+                        <p></p>
+                    </div>
+                </div>
+                <!--Contact Two Single End-->
+                <!--Contact Two Single Start-->
+                <div class="col-xl-4 col-lg-4">
+                    <div class="contact-two__single">
+                        <div class="contact-two__icon">
+                            <span class="icon-location"></span>
+                        </div>
+                        <p>İletişim Adres</p>
+                        <h3>{{ config("contact.address") }}</h3>
+                    </div>
+                </div>
+                <!--Contact Two Single End-->
+            </div>
             <div class="contact-three__inner">
                 <div class="row">
                     <div class="col-xl-6">
