f31718a10c691e6bd32e982dd531826a9150d680
Kuzey Oku Software <171201010@ogrenci.karatekin.edu.tr>
2024-08-31 00:50:34 +0300

    X

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Slider;
 use App\Services\SeoService;
 use App\Services\ContactService;
 use App\Http\Requests\ContactRequest;
@@ -12,7 +13,8 @@ class ContactController extends Controller
     public function index()
     {
         SeoService::set(["title" => __("front/contact.title")]);
-        return view('contact');
+        $slider = Slider::active()->order()->get();
+        return view('contact', compact('slider'));
     }
 
     public function send(ContactRequest $request)
