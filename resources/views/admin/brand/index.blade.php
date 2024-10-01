51dfda4c67b5aa17e184f4be7efec8b3e68dee9f
Kuzey Oku Software <171201010@ogrenci.karatekin.edu.tr>
2024-10-01 22:11:51 +0300

    f

--- resources/views/admin/brand/index.blade.php
+++ resources/views/admin/brand/index.blade.php
@@ -15,7 +15,7 @@
             @forelse ($items as $item)
                 <tr>
                     <td>{{ $item->id }}</td>
-                    <td><img class="table-image" src="{{ $item->getFirstMediaUrl("image") }}"></td>
+                    <td><img class="table-image" src="{{ $item->image }}"></td>
                     <td>{{ $item->created_at->diffForHumans() }}</td>
                     <td>{{ $item->updated_at->diffForHumans() }}</td>
                     @include(themeView('admin', 'layout.status'))
--- resources/views/layout/slider.blade.php
+++ resources/views/layout/slider.blade.php
@@ -1,6 +1,5 @@
 <section class="main-slider">
     <div class="main-slider__carousel owl-carousel owl-theme">
-
         @foreach ($slider as $slider)
             <div class="item">
                 <img src="{{ $slider->getFirstMediaUrl('cover') }}" alt="">
